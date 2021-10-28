49377427b8cd939fda63752821ab01da28e4a9c8
feliujosefina <mjosefinafe@gmail.com>
2021-10-28 19:04:39 -0300

    homework

--- 01-JSI/homework/homework.js
+++ 01-JSI/homework/homework.js
@@ -100,14 +100,14 @@ return nombre + " " + apellido
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   // Tu código:
-
+return lado + lado + lado + lado
 }
 
 function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   // Tu código:
-  
+  return euro * 1.20
 }
 
 
@@ -116,7 +116,11 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Tu código:
-
+if (letra.length > 1) {
+  return "Dato incorrecto";
+} else if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+  return 'Es vocal';
+}
 }
 
 function obtenerMayor(x, y) {
@@ -124,7 +128,11 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-
+if (x > y) {
+  return x;
+} else {
+  return y
+}
 }
 
 /*
@@ -147,21 +155,40 @@ function colors(color) {
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
   // Tu código:
-
+switch (color) {
+  case "blue":
+    return "This is blue";
+    case "red":
+      return "This is red";
+      case "green":
+        return "This is green";
+        case "orange":
+          return "This is orange";
+          default:
+            return "Color not found";
+}
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-
+if (numero === 10 || numero === 5) {
+  return true
+} else {
+  return false
+}
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-
+if (numero < 50 && numero > 20) {
+  return true
+} else {
+  return false
+}
 }
 
 function esPrimo(numero) {
@@ -171,7 +198,13 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
   // Tu código:
-
+  if (numero === 0 || numero === 1) {
+    return false;
+  }
+  for (var i = 2; i < numero; i++) {
+    if(numero % i === 0) return false;
+  }
+  return true;
 }
 
 function doWhile(numero) {
@@ -179,7 +212,12 @@ function doWhile(numero) {
   //Retornar el valor final.
   //Pueden usar un while o un bucle do ... while (Investigarlo si quieren hacerlo de la segunda forma)
   // Tu código:
-  
+  var times = 0;
+  while (times < 8) {
+    numero = numero + 5;
+    times++;
+  }
+  return numero;
 }
 
 
